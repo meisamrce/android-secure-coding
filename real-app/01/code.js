dc6cd681900363ed903bd30ce31f936593cb4257
Meysam Monsef <meisamrce@gmail.com>
2022-07-30 17:28:38 +0430

    add real app

---
+++ real-app/01/code.js
@@ -0,0 +1,19 @@
+console.log("Script loaded successfully ");
+Java.perform(function () {
+    console.log("Inside java perform function");
+    var rootbeer = Java.use("com.scottyab.rootbeer.RootBeer");
+    rootbeer.isRooted.implementation = function () {
+        console.log('rootbeer is bypass!');
+        return false;
+    }
+
+    var DeviceHelper = Java.use("ir.co.ecd.deca.utils.DeviceHelper");
+    DeviceHelper.isEmulator.implementation = function () {
+        console.log('isEmulator is bypass!');
+        return false;
+    }
+
+
+    
+
+});
