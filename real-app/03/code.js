77fb995d1af3da07c5a5e11ab248fe5f4254992a
Meysam Monsef <meisamrce@gmail.com>
2022-08-17 08:14:27 +0430

    add app

---
+++ real-app/03/code.js
@@ -0,0 +1,18 @@
+console.log("Script loaded successfully ");
+Java.perform(function () {
+    console.log("Inside java perform function");
+    var b = Java.use("db.b");
+    b.n.implementation = function () {
+        console.log('n');
+        var n = this.n();
+        console.log(n);
+        return false;
+    }
+
+    var x0 = Java.use("da.x0");
+    x0.V1.implementation = function () {
+        console.log('V1');
+        return false;
+    }
+
+});
