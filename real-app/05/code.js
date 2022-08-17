e9ee12938b56af9f2fe7ca84e051889c34268081
Meysam Monsef <meisamrce@gmail.com>
2022-08-17 10:19:57 +0430

    add new app real

---
+++ real-app/05/code.js
@@ -0,0 +1,35 @@
+console.log("Script loaded successfully ");
+Java.perform(function () {
+    console.log("Inside java perform function");
+    var count = 0;
+    setTimeout(function() {
+        console.log('setTimeout is call');
+        Java.choose("ata.zeki.chat._01_start",{
+            onMatch:function(instance){
+                console.log(instance);
+                Java.scheduleOnMainThread(function () {
+                    console.log("hook :)");
+                    console.log(count);
+                    instance.finish.overload().implementation = function()
+                    {
+                        console.log('finish is call!');
+                        count++;
+                        console.log(count);
+                        if(count > 2)
+                        {   
+                            instance.finish.overload().call(this);
+                        }
+                    }    
+                    
+                    instance.isRooted.overload().implementation = function()
+                    {
+                        console.log('isRooted!');
+                        return false;
+                    }
+                });
+            },
+            onComplete:function (){}
+        }); 
+    },5000);
+
+});
