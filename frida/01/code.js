9811ad682a79013f1db3dd7789a2eca0ee360568
Meisam Monsef <meisamrce@gmail.com>
2024-11-10 15:50:34 +0330

    f

--- frida/01/code.js
+++ frida/01/code.js
@@ -4,11 +4,5 @@ Java.perform(function x() {
     //get a wrapper for our class
     var my_class = Java.use("com.example.frida1.MainActivity");
     //replace the original implmenetation of the function `fun` with our custom function
-    my_class.sum.implementation = function (x, y) {
-        //print the original arguments
-        console.log("original call: sum(" + x + ", " + y + ")");
-        //call the original implementation of `sun` with args (5,10)
-        var ret_value = this.sum(5,10);
-        return ret_value;
-    }
+
 });
